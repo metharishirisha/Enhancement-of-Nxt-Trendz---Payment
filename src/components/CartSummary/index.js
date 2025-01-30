1f8f12e0b497e2d45a67758398598514e24b8a92
metharishirisha <metharishirisha7@gmail.com>
2025-01-30 16:46:17 +0000

    Solution

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -1,4 +1,6 @@
-// Write your code here
+import Popup from 'reactjs-popup'
+import Payment from '../Payment'
+
 import CartContext from '../../context/CartContext'
 
 import './index.css'
@@ -7,19 +9,31 @@ const CartSummary = () => (
   <CartContext.Consumer>
     {value => {
       const {cartList} = value
-      const priceArray = cartList.map(
-        eachValue => eachValue.price * eachValue.quantity,
+      const items = cartList.length
+      const totalPrice = cartList.reduce(
+        (acc, item) => acc + item.quantity * item.price,
+        0,
       )
-      const priceSum = priceArray.reduce((a, b) => a + b, 0)
       return (
         <div className="cart-summary-container">
-          <h1 className="total-order">
-            Order Total: <span className="price-total">Rs {priceSum}/- </span>
-          </h1>
-          <p className="total-quantity">{cartList.length} Items in cart</p>
-          <button type="button" className="check-out">
-            Checkout
-          </button>
+          <div className="cart-summary-card">
+            <h1 className="cart-items-total-price">
+              Order Total: <span>RS {totalPrice}/-</span>
+            </h1>
+            <p className="cart-items-count">{items} Items in cart</p>
+
+            <Popup
+              modal
+              trigger={
+                <button className="checkout-btn" type="button">
+                  Checkout
+                </button>
+              }
+              position="top left"
+            >
+              {close => <Payment close={close} />}
+            </Popup>
+          </div>
         </div>
       )
     }}
